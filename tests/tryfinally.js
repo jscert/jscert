c80b4026f83ebcecc579a71427788c7cef20a8ba
Alan Schmitt <alan.schmitt@polytechnique.org>
2013-01-24 11:14:15 +0100

    adding a few tests for break and try/finally

--- tests/tryfinally.js
+++ tests/tryfinally.js
@@ -11,5 +11,23 @@
 //////////////////////////////////////////////////////////////////////////////
 //CHECK#1
 if (eval("try {3} finally {4}") !== 3) {
-    $ERROR('#1: (try {3} finally {4} !== 3)')
+    $ERROR('#1: (try {3} finally {4}) !== 3')
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//CHECK#2
+if (eval("while(true){try {3; break} finally {4}}") !== 3) {
+    $ERROR('#2: (while(true){try {3; break} finally {4}}) !== 3')
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//CHECK#3
+if (eval("while(true){try {3} finally {4; break}}") !== 3) {
+    $ERROR('#3: (while(true){try {3} finally {4; break}}) !== 4')
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//CHECK#4
+if (eval("while(true){try {3; break} finally {4; break}}") !== 3) {
+    $ERROR('#4: (while(true){try {3; break} finally {4; break}}) !== 4')
 }
