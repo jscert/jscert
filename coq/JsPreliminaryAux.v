a5c4466e0b300d4d81f99af1da53d5fd05e2bc96
Martin Bodin <martin.bodin@ens-lyon.org>
2013-01-31 19:03:07 +0100

    Trying to make things compiles.

--- coq/JsPreliminaryAux.v
+++ coq/JsPreliminaryAux.v
@@ -13,15 +13,28 @@ Implicit Type l : object_loc.
 Implicit Type w : prim.
 Implicit Type v : value.
 Implicit Type r : ref.
+Implicit Type B : builtin.
 Implicit Type T : type.
 
+Implicit Type rt : restype.
+Implicit Type rv : resvalue.
+Implicit Type lab : label.
+Implicit Type labs : label_set.
+Implicit Type R : res.
+Implicit Type o : out.
+
 Implicit Type x : prop_name.
+Implicit Type str : strictness_flag.
 Implicit Type m : mutability.
-Implicit Type A : prop_attributes.
-Implicit Type An : prop_descriptor.
+Implicit Type Ad : attributes_data.
+Implicit Type Aa : attributes_accessor.
+Implicit Type A : attributes.
+Implicit Type Desc : descriptor.
+Implicit Type D : full_descriptor.
+
 Implicit Type L : env_loc.
-Implicit Type E : env_record. (* suggested R *)
-Implicit Type D : decl_env_record.
+Implicit Type E : env_record.
+Implicit Type Ed : decl_env_record.
 Implicit Type X : lexical_env.
 Implicit Type O : object.
 Implicit Type S : state.
@@ -44,46 +57,10 @@ Proof.
   destruct x; destruct y; simpls~; typeclass.
 Qed.
 
-Global Instance some_compare_dec : forall (A : Type) F (x y : option A),
-  (forall u v : A, Decidable (F u v)) ->
-  Decidable (some_compare F x y).
-Proof.
-  introv D.
-  destruct x; destruct y; simpls~; typeclass.
-Qed.
-
-Global Instance value_same_dec : forall v1 v2,
-  Decidable (value_same v1 v2).
-Proof.
-  introv. unfolds value_same.
-  sets_eq T1 E1: (type_of v1). sets_eq T2 E2: (type_of v2).
-  cases_if; try typeclass.
-  destruct T1; try typeclass.
-  repeat cases_if; try typeclass.
-Qed.
-
-Global Instance prop_attributes_contains_dec : forall oldpf newpf,
-  Decidable (prop_attributes_contains oldpf newpf).
-Proof.
-  introv. destruct oldpf. destruct newpf. simpl.
-  repeat apply and_decidable; typeclass.
-Qed.
-
-Lemma value_same_self : forall v,
-  value_same v v.
-Proof.
-  introv. unfolds value_same. sets_eq T E: (type_of v).
-  cases_if; tryfalse. destruct~ T.
-  repeat cases_if~.
-   skip. (* Where is the lemma stating that zero <> neg_zero in JsNumber? *)
-   skip.
-Qed.
-
 Lemma if_some_value_then_same_self : forall vo,
   if_some_value_then_same vo vo.
 Proof.
-  introv. unfolds. unfolds. destruct~ vo.
-   apply value_same_self.
+  introv. unfolds. unfolds. destruct~ vo. reflexivity.
 Qed.
 
 Lemma if_some_bool_then_same_self : forall bo,
@@ -92,61 +69,69 @@ Proof.
   introv. destruct bo; simpls~.
 Qed.
 
-Lemma prop_attributes_contains_self : forall A,
-  prop_attributes_contains A A.
-Proof.
-  introv. destruct A. simpl.
-  splits; (apply if_some_value_then_same_self
-    || apply if_some_bool_then_same_self).
-Qed.
+
+(**************************************************************)
+(** ** Type [attributes_data] *)
+
+Global Instance attributes_data_comparable : Comparable attributes_data.
+Admitted. (* TODO *)
+
+
+(**************************************************************)
+(** ** Type [attributes_accessor] *)
+
+Global Instance attributes_accessor_comparable : Comparable attributes_accessor.
+Admitted. (* TODO *)
 
 
 (**************************************************************)
 (** ** Type [attributes] *)
 
-(* --TODO: used?
-
-  (** Boolean comparison *)
-
-  Definition prop_attributes_compare A1 A2 :=
-    decide (prop_attributes_contains A1 A2 /\ prop_attributes_contains A2 A1).
-
-  (** Decidable comparison *)
-
-  Global Instance prop_attributes_comparable : Comparable prop_attributes.
-  Proof.
-    applys (comparable_beq prop_attributes_compare). intros x y.
-    skip. (* TODO:  The magical tactic here does not work as it requires to destruct all option types in the context before doing all those congruence stuff. *)
-  Qed.
-
---*)
-
-
-(**************************************************************)
-(** ** Type [prop_descriptor] *)
-
-(** Inhabitants **)
-
-Global Instance prop_descriptor_inhab : Inhab prop_descriptor.
-Proof. apply (prove_Inhab prop_descriptor_undef). Qed.
-
 (** Boolean comparison *)
 
-Definition prop_descriptor_compare An1 An2 :=
-  match An1, An2 with
-  | prop_descriptor_undef, prop_descriptor_undef => true
-  | prop_descriptor_some A1, prop_descriptor_some A2 => decide (A1 = A2)
+Definition attributes_compare A1 A2 :=
+  match A1, A2 with
+  | attributes_data_of Ad1, attributes_data_of Ad2 => decide (Ad1 = Ad2)
+  | attributes_accessor_of Aa1, attributes_accessor_of Aa2 => decide (Aa1 = Aa2)
   | _, _ => false
   end.
 
 (** Decidable comparison *)
 
-Global Instance prop_descriptor_comparable : Comparable prop_descriptor.
+Global Instance attributes_comparable : Comparable attributes.
 Proof.
-  applys (comparable_beq prop_descriptor_compare). intros x y.
+  applys (comparable_beq attributes_compare). intros x y.
   destruct x; destruct y; simpl; rew_refl; iff;
    tryfalse; auto; try congruence.
 Qed.
+
+
+(**************************************************************)
+(** ** Type [full_descriptor] *)
+
+(** Inhabitants **)
+
+Global Instance full_descriptor_inhab : Inhab full_descriptor.
+Proof. apply (prove_Inhab full_descriptor_undef). Qed.
+
+(** Boolean comparison *)
+
+Definition full_descriptor_compare An1 An2 :=
+  match An1, An2 with
+  | full_descriptor_undef, full_descriptor_undef => true
+  | full_descriptor_some A1, full_descriptor_some A2 => decide (A1 = A2)
+  | _, _ => false
+  end.
+
+(** Decidable comparison *)
+
+Global Instance full_descriptor_comparable : Comparable full_descriptor.
+Proof.
+  applys (comparable_beq full_descriptor_compare). intros x y.
+  destruct x; destruct y; simpl; rew_refl; iff;
+   tryfalse; auto; try congruence.
+Qed.
+
 
 (**************************************************************)
 (** ** Type [ref_kind] *)
@@ -182,50 +167,34 @@ Proof. typeclass. Qed.
 (**************************************************************)
 (** ** Some decidable instances *)
 
-Global Instance prop_descriptor_is_data_dec : forall An,
-  Decidable (prop_descriptor_is_data An).
+Global Instance descriptor_is_data_dec : forall Desc,
+  Decidable (descriptor_is_data Desc).
 Proof.
-  introv. destruct An; try typeclass.
-  apply neg_decidable. apply and_decidable; typeclass.
+  introv. apply neg_decidable.
+  apply and_decidable; typeclass.
 Qed.
 
-Global Instance prop_descriptor_is_accessor_dec : forall An,
-  Decidable (prop_descriptor_is_accessor An).
+Global Instance descriptor_is_accessor_dec : forall Desc,
+  Decidable (descriptor_is_accessor Desc).
 Proof.
-  introv. destruct An; try typeclass.
-  apply neg_decidable. apply and_decidable; typeclass.
+  introv. apply neg_decidable.
+  apply and_decidable; typeclass.
 Qed.
 
-Global Instance prop_descriptor_is_generic_dec : forall An,
-  Decidable (prop_descriptor_is_generic An).
-Proof.
-  introv. destruct An; try typeclass.
-  repeat (apply and_decidable || apply neg_decidable); try typeclass.
-Qed.
-
-Global Instance prop_descriptor_fully_populated_data_dec : forall An,
-  Decidable (prop_descriptor_fully_populated_data An).
-Proof.
-  introv. destruct An; try typeclass.
-  repeat apply and_decidable; typeclass.
-Qed.
-
-Global Instance prop_descriptor_fully_populated_accessor_dec : forall An,
-  Decidable (prop_descriptor_fully_populated_accessor An).
-Proof.
-  introv. destruct An; try typeclass.
-  repeat apply and_decidable; typeclass.
-Qed.
+Global Instance descriptor_is_generic_dec : forall Desc,
+  Decidable (descriptor_is_generic Desc).
+Proof. typeclass. Qed.
 
 Global Instance res_label_in_dec : forall R labs,
   Decidable (res_label_in R labs).
 Proof.
   introv. unfold res_label_in. destruct (res_label R).
-   skip. (* TODO *)
+   skip. (* TODO:  set manipulations *)
    typeclass.
 Qed.
 
 Global Instance prepost_unary_op_dec : forall op,
   Decidable (prepost_unary_op op).
 Proof. introv. destruct op; typeclass. Qed.
+
 
