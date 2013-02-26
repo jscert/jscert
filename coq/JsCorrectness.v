4fbc8c82d6cb63ee0c1415d8dffd8b6fbe22deff
Martin Bodin <martin.bodin@ens-lyon.org>
2013-02-26 16:29:03 +0100

    Working on tactics.

--- coq/JsCorrectness.v
+++ coq/JsCorrectness.v
@@ -88,37 +88,72 @@ Proof.
    lets (S&R&E): (rm P). intro A. substs. inverts A.
 Qed.
 
-Ltac unmonad_if_ter :=
+
+(* [need_ter I NT E res S R] generates two goals:
+   * one where [not_ter res]
+   * one where [res = out_ter S R] *)
+Ltac need_ter I NT E res S R :=
+  tests NT: (not_ter res); [|
+    rewrite not_ter_forall in NT;
+     lets (S&R&E): (rm NT); rewrite E in I; clear E; simpl in I ].
+
+(* Unfolds one monadic contructor in the environnement. *)
+Ltac if_unmonad :=
+  let NT := fresh "NT" in
+  let E := fresh "Eq" in
+  let S := fresh "S" in
+  let R := fresh "R" in
   match goal with
-  | I: if_ter ?res ?F = result_normal (out_ter ?S0 ?R0) |- ?g =>
-    let NT := fresh "NT" in
-    tests NT: (not_ter res); [
-      let G := fresh "G" in
-      cuts G: (res = out_ter S0 R0 -> g); [
-        apply G; inverts NT; inverts I
-      | let E := fresh "Eq" in
-        introv E; clear NT ]
-     | rewrite not_ter_forall in NT;
-       let S := fresh "S" in
-       let R := fresh "R" in
-       let E := fresh "Eq" in
-       lets (S&R&E): (rm NT); rewrite E in I; clear E; simpl in I ]
+
+  | I: if_success_state ?rv ?res ?K
+      = result_normal (out_ter ?S0 ?R0) |- ?g =>
+    need_ter I NT E res S R; [
+       try (inverts NT; inverts I; fail)
+      | let C1 := fresh "C" in
+        let C2 := fresh "C" in
+        let C3 := fresh "C" in
+        cuts C1: (res_type R = restype_normal -> g); [
+        cuts C2: (res_type R = restype_break -> g); [
+        cuts C3: ((res_type R = restype_continue
+            \/ res_type R = restype_return
+            \/ res_type R = restype_throw) ->
+          out_ter S (res_overwrite_value_if_empty rv R)
+            = out_ter S0 R0 -> g); [
+        destruct (res_type R); [ apply C1 | apply C2
+          | apply C3 | apply C3 | apply C3 ];
+          (reflexivity || (inverts~ I; fail) || idtac)
+        | clear C1 C2] | clear C1] |];
+          let RT := fresh "RT" in
+          introv RT ; (rewrite RT in I || (clear I; introv I)) ]
+
+  | I: if_ter ?res ?K
+      = result_normal (out_ter ?S0 ?R0) |- ?g =>
+    need_ter I NT E res S R; [
+       try (inverts NT; inverts I; fail) |]
+
   end.
 
 Parameter gT : Prop.
 
-Lemma test : forall res S0 R0,
+Lemma test : forall res res0 S0 R0,
   if_ter res (fun S R =>
-    result_stuck) = out_ter S0 R0 -> gT.
+    if_success_state resvalue_empty res0 (fun S' rv =>
+      result_stuck))
+  = out_ter S0 R0 -> gT.
 Proof.
   introv I.
 
-  unmonad_if_ter.
+  if_unmonad.
+  cuts P: (result -> Prop).
+  cuts: (P res0).
+  if_unmonad.
 
   skip.
   skip.
 
-Qed.
+  Set Printing All. idtac.
+
+Admitted.
 
 
 (**************************************************************)
