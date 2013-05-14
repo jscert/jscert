8648f8833a4003afebcd4c74e282f5fd131c42a8
Martin Bodin <martin.bodin@ens-lyon.org>
2013-05-14 14:55:11 +0200

    Cleaning the correctness.

--- coq/JsCorrectness.v
+++ coq/JsCorrectness.v
@@ -71,105 +71,13 @@ Qed.
 
 
 (**************************************************************)
-(** Monadic constructors *)
-(* TODO:  Remove this section once I'll be sure it's useless. *)
-
-Inductive not_ter : result -> Prop :=
-  | not_ter_div : not_ter out_div
-  | not_ter_stuck : not_ter result_stuck
-  | not_ter_bottom : not_ter result_bottom.
-
-Lemma not_ter_forall : forall res,
-  ~ not_ter res <-> exists S R, res = out_ter S R.
-Proof.
-  iff P.
-   destruct res; try (false P; constructors).
-    destruct o. false P; constructors. repeat eexists.
-   lets (S&R&E): (rm P). intro A. substs. inverts A.
-Qed.
-
-
-(*
-(* [need_ter I NT E res S R] generates two goals:
-   * one where [not_ter res]
-   * one where [res = out_ter S R] *)
-Ltac need_ter I NT E res S R k :=
-  let res0 := fresh "res" in
-  let EQres0 := fresh I in
-  sets_eq res0 EQres0: res;
-  symmetry in EQres0;
-  tests NT: (not_ter res0); [
-      try solve [ inverts_not_ter NT I ]
-    | rewrite not_ter_forall in NT;
-      lets (S&R&E): (rm NT); rewrite E in I; clear E; simpl in I; k ].
-*)
-
-(*
-(* Unfolds one monadic contructor in the environnement, calling the
-  continuation when getting a [not_ter]. *)
-Ltac if_unmonad k :=
-(*
-  let NT := fresh "NT" in
-  let E := fresh "Eq" in
-  let S := fresh "S" in
-  let R := fresh "R" in
-*)
-  match goal with
-
-(*
-  | I: if_success_value ?runs ?C ?rev ?K = ?res |- ?g =>
-    unfold if_success_value in I; if_unmonad
-
-  | I: if_success ?rev ?K = ?res |- ?g =>
-    unfold if_success in I; if_unmonad
-
-  | I: if_success_state ?rv ?res ?K = ?res' |- ?g =>
-    need_ter I NT E res S R ltac:(
-      let C := fresh "C" in
-      asserts C: ((res_type R = restype_normal -> g)
-        /\ (res_type R = restype_break -> g)
-        /\ ((res_type R = restype_continue
-          \/ res_type R = restype_return
-          \/ res_type R = restype_throw) ->
-        result_normal (out_ter S (res_overwrite_value_if_empty rv R))
-          = res' -> g)); [
-          splits;
-          let RT := fresh "RT" in
-          introv RT; first [ rewrite RT in I | clear I; introv I ]
-      | let C1 := fresh "C" in
-        let C2 := fresh "C" in
-        let C3 := fresh "C" in
-        lets (C1&C2&C3): (rm C);
-        destruct (res_type R); [ apply C1 | apply C2
-          | apply C3 | apply C3 | apply C3 ];
-          first [ reflexivity | inverts~ I; fail | idtac] ])
-
-  | I: if_ter ?res ?K = ?res' |- ?g =>
-    need_ter I NT E res S R ltac:idtac
-
-  | I: result_normal (out_ter ?S1 ?R1)
-      = result_normal (out_ter ?S0 ?R0) |- ?g =>
-    inverts~ I
-
-  | I: out_ter ?S1 ?R1 = out_ter ?S0 ?R0 |- ?g =>
-    inverts~ I
-*)
-
-  | I : if_bool_option_result ?op ?K1 ?K2 = out_ter ?S0 ?R0 =>
-    
-
-  end.
-*)
-
-
-(**************************************************************)
 (** Correctness Properties *)
 
 Definition follow_spec {T Te : Type}
     (conv : T -> Te) (red : state -> execution_ctx -> Te -> out -> Prop)
-    (run : state -> execution_ctx -> T -> result) := forall S C (e : T) o,
-  run S C e = o ->
-  red S C (conv e) o.
+    (run : state -> execution_ctx -> T -> result) := forall S C (e : T) S' res,
+  run S C e = out_ter S' res ->
+  red S C (conv e) (out_ter S' res).
 
 Definition follow_expr := follow_spec expr_basic red_expr.
 Definition follow_stat := follow_spec stat_basic red_stat.
@@ -186,17 +94,49 @@ Definition follow_call_full l vs :=
     red_expr.
 
 
-Record runs_type_correct runs (*run_elements*) :=
+Record runs_type_correct runs run_elements :=
   make_runs_type_correct {
     runs_type_correct_expr : follow_expr (runs_type_expr runs);
     runs_type_correct_stat : follow_stat (runs_type_stat runs);
-    runs_type_correct_prog : follow_prog (runs_type_prog runs)(*;
-    runs_type_correct_elements : follow_elements run_elements;
-    runs_type_correct_call : follow_call (runs_type_call runs);
-    runs_type_correct_call_full : follow_call_full (runs_type_call_full runs)*)
+    runs_type_correct_prog : follow_prog (runs_type_prog runs);
+    runs_type_correct_elements : forall rv,
+      follow_elements rv (fun S C => run_elements S C rv);
+    runs_type_correct_call : forall vs,
+      follow_call vs (fun S C B => runs_type_call runs S C B vs);
+    runs_type_correct_call_full : forall l vs,
+      follow_call_full l vs (fun S C vthis => runs_type_call_full runs S C l vthis vs)
   }.
 
 
+
+(**************************************************************)
+(** Monadic constructors *)
+
+(* Unfolds one monadic contructor in the environnement, calling the
+  continuation when getting an unsolved non-terminating reduction. *)
+Ltac if_unmonad k :=
+  match goal with
+
+  | I: out_ter ?S1 ?R1 = out_ter ?S0 ?R0 |- ?g =>
+    inverts~ I
+
+  | I : if_ter ?o ?K = ?o0 |- ?g =>
+    unfold if_ter in I;
+    let o' := fresh "o" in
+    let o'' := fresh "o" in
+    let Eqo' := fresh "Eq" o' in
+    sets_eq <- o' Eqo': o;
+    destruct o' as [o''| |]; cbv beta in I;
+      [destruct o'';
+        [ k I |]
+      | k I | k I]
+
+  | I : if_success_state ?rv ?o ?K = ?o0 |- ?g =>
+    unfold if_success_state in I;
+    if_unmonad k;
+    idtac
+
+  end.
 
 (**************************************************************)
 (** Operations on objects *)
@@ -222,13 +162,13 @@ Qed.
 (** ** Main theorems *)
 
 Theorem runs_correct : forall num,
-  runs_type_correct (runs num).
+  runs_type_correct (runs num) (run_elements num).
 Proof.
 
   induction num.
    constructors; introv R; inverts R.
 
-   lets [IHe IHs IHp]: (rm IHnum).
+   lets [IHe IHs IHp IHel IHc IHcf]: (rm IHnum).
    constructors.
 
    (* run_expr *)
@@ -238,74 +178,28 @@ Proof.
    skip.
 
    (* run_prog *)
+   intros S C p S' res R. destruct p as [str es]. simpls.
+   forwards RC: IHel R. apply~ red_prog_prog.
+
+   (* run_elements *)
+   intros rv S C es S' res R. destruct es; simpls.
+    inverts R. apply~ red_prog_1_nil.
+    destruct e.
+     unfold if_success_state in R. if_unmonad ltac:(fun I => try inverts I).
+       destruct (res_type r). skip.
+       inverts R. forwards RC: IHs Eqo.
+         applys~ red_prog_1_cons_stat RC.
+         apply~ red_prog_2. skip.
+         apply red_prog_abort. reflexivity.
+       skip.
+     forwards RC: IHel R. apply~ red_prog_1_cons_funcdecl.
+
+   (* run_call *)
+   skip.
+
+   (* run_call_full *)
+   intros l vs S C v S' res R. simpls.
    skip.
 
 Qed.
-
-(*
-  (* run_prog_correct *)
-  destruct num. auto*.
-  intros S0 C p o R. destruct p as [str es].
-  forwards RC: run_elements_correct R.
-  apply~ red_prog_prog.
-
-  (* run_stat_correct *)
-  destruct num. auto*.
-  intros S0 C t o R. destruct t.
-
-   (* stat_expr *)
-   simpls. repeat if_unmonad.
-    inverts_not_ter NT R. forwards: run_expr_correct R2.
-     apply red_stat_abort. (* TODO:  This could be turned into a tactic. *)
-      skip. (* Needs implementation of [out_of_ext_stat]. *)
-      constructors.
-      intro A. inverts A.
-    inverts_not_ter NT R.
-  (* forwards: run_expr_correct R2.
-     apply red_stat_abort. (* TODO:  This could be turned into a tactic. *)
-      skip. (* Needs implementation of [out_of_ext_stat]. *)
-      constructors.
-      intro A. inverts A.*)
-    skip.
-    skip.
-    skip.
-    skip.
-    skip.
-    skip.
-    skip.
-    skip.
-
-   (* stat_label *)
-   skip.
-
-   (* TODO: Complete *)
-   skip.
-   skip.
-   skip.
-   skip.
-   skip.
-   skip.
-   skip.
-   skip.
-   skip.
-   skip.
-   skip.
-   skip.
-   skip.
-   skip.
-
-  (* run_expr_correct *)
-  skip.
-
-  (* run_elements_correct *)
-  skip.
-
-  (* run_call_correct *)
-  skip.
-
-  (* run_call_full_correct *)
-  skip.
-
-Admitted.
-*)
 
