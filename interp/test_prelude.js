9e98311b6d7e4d9b407a07980f75c3be1ed1418f
Gareth Smith <gds@doc.ic.ac.uk>
2013-05-31 15:24:08 +0100

    Updated test_prelude to support test262

--- interp/test_prelude.js
+++ interp/test_prelude.js
@@ -1,4 +1,7 @@
+// Martin's original error function, which sets the secret state.
 function $ERROR (str) {
   __$ERROR__ = str
 }
 
+// Gareth's addition, which brings it up to Test262 conformance. Hopefully.
+t262 = {$ERROR:$ERROR , TestFailureError: function(err){return {__$ERROR__:err}}}
