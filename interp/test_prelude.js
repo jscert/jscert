04f6f8888ffd847106e25e794789c6cb5310eb4d
Gareth Smith <gds@doc.ic.ac.uk>
2013-06-03 15:26:41 +0100

    Better error reporting for test262.

--- interp/test_prelude.js
+++ interp/test_prelude.js
@@ -1,8 +1,12 @@
-// Martin's original error function, which sets the secret state.
+// Martin's original error function, which sets the secret state. Updated to keep more than one error message.
 function $ERROR (str) {
-  __$ERROR__ = str
+    try {
+        __$ERROR__ = __$ERROR__ + " | " + str
+    }
+    catch(ex) {
+        __$ERROR__ = str
+    }
 }
 
 // Gareth's addition, which brings it up to Test262 conformance. Hopefully.
 t262 = {$ERROR:$ERROR , TestFailureError: function(err){return {__$ERROR__:err}}}
-
