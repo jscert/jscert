5825548c1bd0814769c3d0bfe6e45566c5485ab2
Alan Schmitt <alan.schmitt@polytechnique.org>
2013-06-11 18:51:44 +0200

    JSRef: This commit fixes the extraction of 32 bits arithmetic (bug #15848)
    The following tests still do not pass:
    
    The following tests failed:
    /Users/schmitta/projets/jscert/src/core/trunk/tests/test262/ch09/9.5/S9.5_A1_T1.js
    /Users/schmitta/projets/jscert/src/core/trunk/tests/test262/ch09/9.5/S9.5_A3.1_T4.js
    /Users/schmitta/projets/jscert/src/core/trunk/tests/test262/ch09/9.5/S9.5_A3.1_T2.js
    /Users/schmitta/projets/jscert/src/core/trunk/tests/test262/ch09/9.5/S9.5_A3.1_T3.js
    /Users/schmitta/projets/jscert/src/core/trunk/tests/test262/ch09/9.5/S9.5_A3.1_T1.js
    /Users/schmitta/projets/jscert/src/core/trunk/tests/test262/ch09/9.6/S9.6_A1.js
    /Users/schmitta/projets/jscert/src/core/trunk/tests/test262/ch09/9.6/S9.6_A3.1_T3.js
    /Users/schmitta/projets/jscert/src/core/trunk/tests/test262/ch09/9.6/S9.6_A3.1_T4.js
    /Users/schmitta/projets/jscert/src/core/trunk/tests/test262/ch09/9.6/S9.6_A3.1_T1.js
    /Users/schmitta/projets/jscert/src/core/trunk/tests/test262/ch09/9.6/S9.6_A3.1_T2.js
    /Users/schmitta/projets/jscert/src/core/trunk/tests/test262/ch09/9.7/S9.7_A3.1_T4.js
    /Users/schmitta/projets/jscert/src/core/trunk/tests/test262/ch11/11.4/11.4.8/S11.4.8_A3_T2.js
    The following tests were abandoned
    /Users/schmitta/projets/jscert/src/core/trunk/tests/test262/ch07/7.7/S7.7_A1.js
    /Users/schmitta/projets/jscert/src/core/trunk/tests/test262/ch08/8.5/8.5.1.js
    
    Note that these all seem to use builtin primitives (String, Number) or the
    valueOf property.

--- coq/JsInterpreterExtraction.v
+++ coq/JsInterpreterExtraction.v
@@ -27,7 +27,57 @@ Extraction Inline epsilon epsilon_def classicT arbitrary indefinite_description 
 (** ** Numerical values *)
 
 (* number *)
-Require Import ExtrOcamlZInt.
+
+Extract Inductive positive => float
+[ "(fun p -> 1. +. (2. *. p))"
+  "(fun p -> 2. *. p)"
+  "1." ]
+"(fun f2p1 f2p f1 p ->
+if p <= 1. then f1 () else if mod_float p 2. = 0. then f2p (p /. 2.) else f2p1 (p /. 2.))".
+
+Extract Inductive Z => float [ "0." "" "(~-.)" ]
+"(fun f0 fp fn z -> if z=0. then f0 () else if z>0. then fp z else fn (~-. z))".
+
+Extract Inductive N => float [ "0." "" ]
+"(fun f0 fp n -> if n=0. then f0 () else fp n)".
+
+Extract Constant Z.add => "(+.)".
+Extract Constant Z.succ => "(+.) 1.".
+Extract Constant Z.pred => "(fun x -> x -. 1.)".
+Extract Constant Z.sub => "(-.)".
+Extract Constant Z.mul => "( *. )".
+Extract Constant Z.opp => "(~-.)".
+Extract Constant Z.abs => "abs_float".
+Extract Constant Z.min => "min".
+Extract Constant Z.max => "max".
+Extract Constant Z.compare =>
+ "fun x y -> if x=y then Eq else if x<y then Lt else Gt".
+
+Extract Constant Pos.add => "(+.)".
+Extract Constant Pos.succ => "(+.) 1.".
+Extract Constant Pos.pred => "(fun x -> x -. 1.)".
+Extract Constant Pos.sub => "(-.)".
+Extract Constant Pos.mul => "( *. )".
+Extract Constant Pos.min => "min".
+Extract Constant Pos.max => "max".
+Extract Constant Pos.compare =>
+ "fun x y -> if x=y then Eq else if x<y then Lt else Gt".
+Extract Constant Pos.compare_cont =>
+ "fun x y c -> if x=y then c else if x<y then Lt else Gt".
+
+Extract Constant N.add => "(+.)".
+Extract Constant N.succ => "(+.) 1.".
+Extract Constant N.pred => "(fun x -> x -. 1.)".
+Extract Constant N.sub => "(-.)".
+Extract Constant N.mul => "( *. )".
+Extract Constant N.min => "min".
+Extract Constant N.max => "max".
+Extract Constant N.div => "(fun x y -> if x = 0. then 0. else floor (x /. y))".
+Extract Constant N.modulo => "mod_float".
+Extract Constant N.compare =>
+ "fun x y -> if x=y then Eq else if x<y then Lt else Gt".
+
+
 Extract Inductive Fappli_IEEE.binary_float => float [
   "(fun s -> if s then (0.) else (-0.))"
   "(fun s -> if s then infinity else neg_infinity)"
@@ -35,15 +85,14 @@ Extract Inductive Fappli_IEEE.binary_float => float [
   "(fun (s, m, e) -> failwith ""FIXME: No extraction from binary float allowed yet."")"
 ].
 
-Extract Constant JsNumber.of_int => "float_of_int".
-Extract Constant JsNumber_to_int => "int_of_float".
+Extract Constant JsNumber.of_int => "fun x -> x".
 
 Extract Constant JsNumber.nan => "nan".
 Extract Constant JsNumber.zero => "0.".
 Extract Constant JsNumber.neg_zero => "(-0.)".
 Extract Constant JsNumber.one => "1.".
 Extract Constant JsNumber.infinity => "infinity".
-Extract Constant JsNumber.neg_infinity => "(-.infinity)".
+Extract Constant JsNumber.neg_infinity => "neg_infinity".
 Extract Constant JsNumber.floor => "floor".
 Extract Constant JsNumber.absolute => "abs_float".
 Extract Constant JsNumber.from_string =>
@@ -73,18 +122,47 @@ Extract Constant JsNumber.sign => "(fun f -> float_of_int (compare f 0.))".
 Extract Constant JsNumber.number_comparable => "(fun n1 n2 -> 0 = compare n1 n2)".
 Extract Constant JsNumber.lt_bool => "(<)".
 
-Extract Constant JsNumber.to_int32 => "int_of_float".
-Extract Constant JsNumber.to_uint32 => "(int_of_float (* TODO:  Replace by the right operation. *))".
-Extract Constant JsNumber.modulo_32 => "(fun x -> x (* TODO:  To be reread. *))".
-Extract Constant JsNumber.int32_bitwise_not => "(lnot)".
-Extract Constant JsNumber.int32_bitwise_and => "(land)".
-Extract Constant JsNumber.int32_bitwise_or => "(lor)".
-Extract Constant JsNumber.int32_bitwise_xor => "(lxor)".
-Extract Constant JsNumber.int32_left_shift => "(lsl)".
-Extract Constant JsNumber.int32_right_shift => "(asr (* TODO:  To be reread. *))".
-Extract Constant JsNumber.uint32_right_shift => "(lsr (* TODO:  To be reread. *))".
+Extract Constant JsNumber.to_int32 => 
+"fun n ->
+  match classify_float n with
+  | FP_normal | FP_subnormal ->
+    let i32 = 2. ** 32. in
+    let i31 = 2. ** 31. in
+    let posint = (if n < 0. then (-1.) else 1.) *. (floor (abs_float n)) in
+    let int32bit =
+      let smod = mod_float posint i32 in
+      if smod < 0. then smod +. i32 else smod
+    in
+    (if int32bit >= i31 then int32bit -. i32 else int32bit)
+  | _ -> 0.". (* TODO do in Coq? Spec is 9.5, p. 47.*)
+Extract Constant JsNumber.to_uint32 =>
+"fun n ->
+  match classify_float n with
+  | FP_normal | FP_subnormal ->
+    let i32 = 2. ** 32. in
+    let posint = (if n < 0. then (-1.) else 1.) *. (floor (abs_float n)) in
+    let int32bit =
+      let smod = mod_float posint i32 in
+      if smod < 0. then smod +. i32 else smod
+    in
+    int32bit
+  | _ -> 0.". (* TODO do in Coq? Spec is 9.6, p47.*)
+Extract Constant JsNumber.modulo_32 => "(fun x -> let r = mod_float x 32. in if x < 0. then r +. 32. else r)".
+Extract Constant JsNumber.int32_bitwise_not => "fun x -> Int32.to_float (Int32.lognot (Int32.of_float x))".
+Extract Constant JsNumber.int32_bitwise_and => "fun x y -> Int32.to_float (Int32.logand (Int32.of_float x) (Int32.of_float y))".
+Extract Constant JsNumber.int32_bitwise_or => "fun x y -> Int32.to_float (Int32.logor (Int32.of_float x) (Int32.of_float y))".
+Extract Constant JsNumber.int32_bitwise_xor => "fun x y -> Int32.to_float (Int32.logxor (Int32.of_float x) (Int32.of_float y))".
+Extract Constant JsNumber.int32_left_shift => "(fun x y -> Int32.to_float (Int32.shift_left (Int32.of_float x) (int_of_float y)))".
+Extract Constant JsNumber.int32_right_shift => "(fun x y -> Int32.to_float (Int32.shift_right (Int32.of_float x) (int_of_float y)))".
+Extract Constant JsNumber.uint32_right_shift => 
+"(fun x y ->
+  let i31 = 2. ** 31. in
+  let i32 = 2. ** 32. in
+  let newx = if x >= i31 then x -. i32 else x in
+  let r = Int32.to_float (Int32.shift_right_logical (Int32.of_float newx) (int_of_float y)) in
+  if r < 0. then r +. i32 else r)".
 
-Extract Constant int_of_char => "int_of_char".
+Extract Constant int_of_char => "(fun c -> float_of_int (int_of_char c))".
 
 Extract Constant prealloc_comparable => "(=)".
 Extract Constant ascii_compare => "(=)".
@@ -101,7 +179,6 @@ Extraction Inline Fappli_IEEE.Bdiv Fappli_IEEE_bits.b64_div.
 
 (* New options for the interpreter to work in Coq 8.4 *)
 Set Extraction AccessOpaque.
-Extract Constant Pos.succ => "Pervasives.succ (* Because of a bug of the extraction printer, we are forced to precise the way we want such objects to be extracted... *)".
 
 (* These parameters are implementation-dependant according to the spec.
    I've chosed some very simple values, but we could choose another thing for them. *)
