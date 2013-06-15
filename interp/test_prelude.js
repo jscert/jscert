3e421d791ab9264e7ccaaa75824e288ce786b389
Gareth Smith <gds@doc.ic.ac.uk>
2013-06-15 13:49:13 +0100

    Added a skeleton TypeError to our test prelude

--- interp/test_prelude.js
+++ interp/test_prelude.js
@@ -10,3 +10,4 @@ function $ERROR (str) {
 
 // Gareth's addition, which brings it up to Test262 conformance. Hopefully.
 t262 = {$ERROR:$ERROR , TestFailureError: function(err){return {__$ERROR__:err}}}
+TypeError = function(err){return {__$ERROR__:err}}
