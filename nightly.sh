5d7b8022e4716e92eed2daab5575ac0a09f9712e
Gareth Smith <gds@doc.ic.ac.uk>
2013-06-26 16:35:31 +0100

    Updated nightly script to generate FIXME list.

--- nightly.sh
+++ nightly.sh
@@ -2,4 +2,5 @@ time ./runtests.py --webreport --title t262LS5nightly --note "Nightly run of all
 cd test_data/query_scripts/
 cabal-dev/bin/update_known_passes
 cabal-dev/bin/make_simple_report --querytype=OnlyInteresting --reportname=NightlyInterstingCases --reportcomment="Checking last night's run for aborts and fails which are not caused by known issues"
+./cabal-dev/bin/make_simple_report --reportname=FIXME --reportcomment="Checking last night's run for cries of 'FIXME'" --querytype=stdoutlike --query="%FIXME%"
 echo "Remember to move known passes file, and move reports to '-latest'"
