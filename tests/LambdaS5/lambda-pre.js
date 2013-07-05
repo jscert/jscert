c0c267c72320fb8550a8609cb399d18b7a699285
Alan Schmitt <alan.schmitt@polytechnique.org>
2013-07-05 15:03:00 +0200

    lambda js tests use 'passed' or 'Passed.'
    they sometimes also use console.log

--- tests/LambdaS5/lambda-pre.js
+++ tests/LambdaS5/lambda-pre.js
@@ -1,7 +1,9 @@
 // The LambdaS5 tests use "print('passed')" to indicate success. We catch these, and record the passing information for later.
 __$JSRef_PASSED__ = false;
 function print(str) {
-    if(str==='passed') {
+    if(str==='passed' || str==='Passed.') {
         __$JSRef_PASSED__ = true
     }
 }
+
+var console = { log : print }
