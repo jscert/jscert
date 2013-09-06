531521f7ad64172a58e76b81abc7b4819cb1a2bb
Lorenz Breidenbach <lorenz.breidenbach12@imperial.ac.uk>
2013-09-06 16:21:44 +0000

    Init: Fix Object.defineProperties.length: was 1, should be 2

--- coq/JsInit.v
+++ coq/JsInit.v
@@ -217,7 +217,7 @@ Definition object_create_function_object :=
 Definition object_define_prop_function_object :=
   object_create_prealloc_call prealloc_object_define_prop 3 Heap.empty.
 Definition object_define_props_function_object :=
-  object_create_prealloc_call  prealloc_object_define_props 1 Heap.empty.
+  object_create_prealloc_call  prealloc_object_define_props 2 Heap.empty.
 Definition object_seal_function_object :=
   object_create_prealloc_call prealloc_object_seal 1 Heap.empty.
 Definition object_freeze_function_object :=
