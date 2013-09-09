a2b91a74b5b78ffc09c3c8e8c33f41e704eff621
Alan Schmitt <alan.schmitt@polytechnique.org>
2013-09-09 10:22:10 +0200

    configuring bisect to be able to run on more than 10000 files

--- runbisect.sh
+++ runbisect.sh
@@ -1,4 +1,8 @@
+nb=0
 for file in `./test262tests`; do
+  prefix=$(( $nb / 9999))
+  nb=$(( $nb + 1 )) 
+  export BISECT_FILE=bisect_$prefix
   interp/run_jsbisect -jsparser interp/parser/lib/js_parser.jar -file $file
 done
 
