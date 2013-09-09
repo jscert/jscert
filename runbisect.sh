ad15b3835c1391bb48dee3e5d7a01735cf95587e
Alan Schmitt <alan.schmitt@polytechnique.org>
2013-09-09 11:58:55 +0200

    using runtests.py for runbisect.sh

--- runbisect.sh
+++ runbisect.sh
@@ -3,7 +3,7 @@ for file in `./test262tests`; do
   prefix=$(( $nb / 9999))
   nb=$(( $nb + 1 )) 
   export BISECT_FILE=bisect_$prefix
-  interp/run_jsbisect -jsparser interp/parser/lib/js_parser.jar -file $file
+  ./runtests.py --interp_path interp/run_jsbisect $file
 done
 
 
