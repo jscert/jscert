367879de104a8247f038f053aed733bc75fc62b7
Conrad <conrad.watt12@imperial.ac.uk>
2014-01-15 21:17:38 +0000

    Change extra_coverage test; preventExtensions not needed for accessor case.

--- tests/extra_coverage/8.12.4-7-a_1.js
+++ tests/extra_coverage/8.12.4-7-a_1.js
@@ -1,14 +1,12 @@
 /**
  * Testing 8.12.4-7-a
- * Accessor property on a prototype with undefined [[Set]] written to with extensions prevented.
- * TODO: ask about factorising
+ * Accessor property on a prototype with undefined [[Set]] written to.
  */
 
 function testcase() {  
     function foo() {};
     foo.prototype = {get bar() {return 0;}};
     var o = new foo();
-    Object.preventExtensions(o);
     o.bar = 1;
     return o.hasOwnProperty("bar") == false && o.bar == 0; 
 }
