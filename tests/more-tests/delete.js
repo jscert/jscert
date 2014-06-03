bef25eedbbaea5633361aa2a3808e55719adfaab
Sergio Maffeis <maffeis@doc.ic.ac.uk>
2014-06-03 09:57:06 +0100

    minor editing of delete test

--- tests/more-tests/delete.js
+++ tests/more-tests/delete.js
@@ -1,17 +1,18 @@
 
 // Testing the SyntaxError exception of the delete operator.
+// If this test fails completely (without $ERROR) then the exception was non-catchable, and most likely it was detected at parse time rather than at run time as it should have been.
 
 var fun = function (){
     "use strict";
-    delete fundfd; // This should throw a SyntaxError exception at call time, not parsing time!
-    $ERROR("#1.1: This code should return a SyntaxError exception.")
+    delete x; // This should throw a SyntaxError exception at call time, not parsing time!
+    $ERROR("#1.1: This code should throw a (catchable) SyntaxError exception.")
 };
 
 try {
     fun ()
 } catch (e){
     if ((e instanceof SyntaxError) !== true){
-        $ERROR("#1.2: This code should return a SyntaxError exception.");
+        $ERROR("#1.2: The exception thrown by this code should have been a SyntaxError exception.");
     }
 }
 
