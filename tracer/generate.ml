514b32cf73c0d996e69815a65790886515d5dc9e
Martin Bodin <martin.bodin@ens-lyon.org>
2014-07-28 17:45:23 +0200

    Minor.

--- tracer/generate.ml
+++ tracer/generate.ml
@@ -1,29 +1,29 @@
 
-let put_contents filename strl =
+let write_file filename strl =
     let oc = open_out filename in
     let () = List.iter (output_string oc) strl in
     close_out oc
 
 
 let load_file f =
-    let ic = open_in f in
+    let ic = try open_in f with _ -> failwith ("Error while reading file “" + f + "”.") in
     let n = in_channel_length ic in
     let s = String.create n in
     really_input ic s 0 n ;
     close_in ic ;
     s
 
-let fileList =
+let fileList = (* Every argument from the third to all the other ones. *)
     let rec aux i =
         if i >= Array.length Sys.argv then []
         else Sys.argv.(i) :: aux (i + 1)
-    in aux 2
+    in aux 3
 
 
 let _ =
-    let fileTrace = Sys.argv.(0) in
-    let fileOut = Sys.argv.(1) in
-    put_contents fileOut (
+    let fileTrace = Sys.argv.(1) in
+    let fileOut = Sys.argv.(2) in
+    write_file fileOut (
             "var tracer_items = [\n" ::
             load_file fileTrace ::
             "\n];\n\nvar tracer_files = {\n" ::
