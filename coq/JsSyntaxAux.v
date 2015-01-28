922f4b25f4ee2a7479218005a3d5881e6e420b5d
Thomas Wood <tw1509@doc.ic.ac.uk>
2015-01-28 22:02:10 +0000

    JsSyntaxAux.v: Fix proofs, I don't know why these were commented out.

--- coq/JsSyntaxAux.v
+++ coq/JsSyntaxAux.v
@@ -267,7 +267,6 @@ Definition prealloc_compare bl1 bl2 :=
 
 Global Instance prealloc_comparable : Comparable prealloc.
 Proof.
-(*
   applys (comparable_beq prealloc_compare). intros x y.
   destruct x.
   destruct y; simpls; rew_refl; iff~; tryfalse.
@@ -329,9 +328,10 @@ Proof.
   destruct y; simpls; rew_refl; iff~; tryfalse; congruence; destruct n; destruct n0; simpls.
   destruct y; simpls; rew_refl; iff~; tryfalse; congruence; destruct n; destruct n0; simpls.
   destruct y; simpls; rew_refl; iff~; tryfalse; congruence; destruct n; destruct n0; simpls.
+  destruct y; simpls; rew_refl; iff~; tryfalse; congruence; destruct n; destruct n0; simpls.
+  destruct y; simpls; rew_refl; iff~; tryfalse; congruence; destruct n; destruct n0; simpls.
   destruct y; simpls; rew_refl; iff~; tryfalse.
   destruct y; simpls; rew_refl; iff~; tryfalse.
-*)
 Admitted. (*faster*)
 
 
@@ -731,9 +731,9 @@ Fixpoint binary_op_compare op1 op2 :=
 
 Global Instance binary_op_comparable : Comparable binary_op.
 Proof.
-  applys (comparable_beq binary_op_compare). (* intros x y.
+  applys (comparable_beq binary_op_compare). intros x y.
   destruct x; destruct y; simpl; rew_refl; iff;
-   tryfalse; auto; try congruence. *)
+   tryfalse; auto; try congruence.
 Admitted. (*faster*)
 
 
